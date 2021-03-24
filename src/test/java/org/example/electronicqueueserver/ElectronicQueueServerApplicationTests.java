998cabb3a29986db25bfc87b40b7d84f79a1974d
ripyatakov <ripyatakov@edu.hse.ru>
2021-03-24 21:18:44 +0300

    create test project

---
+++ src/test/java/org/example/electronicqueueserver/ElectronicQueueServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.electronicqueueserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicQueueServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
